869eb69041e2c8eb513b559419cc8cf3ca77cebb
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-05 16:09:04 -0400

    created set up for redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import {createRoot} from 'react-dom/client'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import store from './store'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
