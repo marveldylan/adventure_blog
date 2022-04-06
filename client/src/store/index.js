d59769ac6b544d1c1a3f2228f886627e61cefe73
Chinwendu <chinwenduukoha@gmail.com>
2022-04-06 18:47:44 -0400

    debug comments map

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,15 +4,16 @@ import thunk from 'redux-thunk'
 import PostReducer from './reducers/PostReducers'
 import PostDetailsReducer from './reducers/PostDetailsReducer'
 import CommentsReducer from './reducers/CommentsReducers'
-
+import CommentDetailsReducer from './reducers/CommentDetailsReducer'
 
 const store = createStore(
-    combineReducers({
-        postState: PostReducer,
-        postDetailsState: PostDetailsReducer,
-        commentsState: CommentsReducer
-    }),
-    composeWithDevTools(applyMiddleware(thunk))
-  )
+  combineReducers({
+    postState: PostReducer,
+    postDetailsState: PostDetailsReducer,
+    commentsState: CommentsReducer,
+    commentDetails: CommentDetailsReducer
+  }),
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
-  export default store
+export default store
