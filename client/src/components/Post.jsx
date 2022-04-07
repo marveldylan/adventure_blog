8bbbfe0f8230379e6c3c3f6b283bde58a434a546
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-06 21:39:39 -0400

    added PostDetails

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -25,7 +25,7 @@ const Post = (props) => {
                     <div key={post._id} className="post-profile">
                     <img className="post-image" src={``} alt="poster" />
                     <h3>{post.title}</h3>
-                    <Link className="view"to={`/blogpost/${post._id}`}>View post</Link>
+                    <Link className="view" to={`/blogpost/${post._id}`}>Write comment</Link>
                     </div>
                     
                 ))
--- client/src/components/PostDetails.jsx
+++ client/src/components/PostDetails.jsx
@@ -1,13 +1,35 @@
 import Comment from "./Comment"
 import WriteComment from "./WriteComment"
-
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+   
+    
+ const mapStateToProps = ({ postDetailsState }) => {
+        return { postDetailsState }
+    }
+    
+const mapDispatchToProps = (dispatch) => {
+        return {
+            fetchPostDetails: (id) => dispatch(LoadPostDetails(id))
+        }
+    }
+    
 const PostDetails = () => {
-
-    return(
-        <div className="post-details-container">
-            <h1>PostDetails is linked</h1>
-        </div>
-    )
-}
-
-export default PostDetails
+        let { id } = useParams()
+    
+        useEffect(() => {
+            props.fetchPostDetails(id)
+        }, [])
+    
+        return (
+            <div className="post-details-container">
+               <p>{ props.postDetailsState.postDetails.post }</p>
+               <Link to='/writecomment'>Write comment</Link>
+            </div>
+        )
+    
+    }
+    
+    export default connect(mapStateToProps, mapDispatchToProps)(PostDetails)
